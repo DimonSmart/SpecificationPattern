1a4a9b45dac4f417e89c9f8c3c7bb2c5374ac472
Dmitry Dorogoy <dorogoj@live.ru>
2023-07-25 17:07:44 +0700

    Formatting fixed

--- EFDBSpecificationProjectTests/SchoolContext.cs
+++ EFDBSpecificationProjectTests/SchoolContext.cs
@@ -12,7 +12,6 @@ namespace EFDBSpecificationProjectTests
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
             optionsBuilder.UseInMemoryDatabase("SchoolDB");
         }
     }
