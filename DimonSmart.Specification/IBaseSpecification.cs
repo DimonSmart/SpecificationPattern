bf9bb71a9c36bc89c450ed30c9a7d71db48888ba
Dmitry Dorogoy <dorogoj@live.ru>
2023-08-07 17:57:08 +0700

    XML Comments updated

--- DimonSmart.Specification/IBaseSpecification.cs
+++ DimonSmart.Specification/IBaseSpecification.cs
@@ -2,52 +2,59 @@
 
 namespace DimonSmart.Specification;
 
+/// <summary>
+/// Interface for building specifications to filter and order data queries.
+/// </summary>
+/// <typeparam name="T">The type of entity being queried.</typeparam>
+/// <typeparam name="TSpecification">The type of specification being built.</typeparam>
 public interface IBaseSpecification<T, out TSpecification> where T : class
     where TSpecification : IBaseSpecification<T, TSpecification>
 {
+    /// <summary>
+    /// Gets the specification data that contains the filter and order information.
+    /// </summary>
     ISpecificationData<T> SpecificationData { get; }
 
     /// <summary>
-    /// Return filtering condition or null if not specified
+    /// Returns the filtering condition or null if not specified.
     /// </summary>
-    /// <returns>Where condition</returns>
+    /// <returns>Where condition as an expression.</returns>
     Expression<Func<T, bool>>? GetWhereExpression();
 
     /// <summary>
-    /// Specify how many elements will be taken from query
+    /// Specifies the number of elements to be taken from the query.
     /// </summary>
-    /// <param name="take"></param>
-    /// <returns></returns>
+    /// <param name="take">The number of elements to take.</param>
+    /// <returns>The current specification with the take condition applied.</returns>
     TSpecification Take(int take);
 
     /// <summary>
-    /// Specify how many elements should be skipped from query
+    /// Specifies the number of elements to be skipped from the query.
     /// </summary>
-    /// <param name="skip"></param>
-    /// <returns></returns>
+    /// <param name="skip">The number of elements to skip.</param>
+    /// <returns>The current specification with the skip condition applied.</returns>
     TSpecification Skip(int skip);
 
     /// <summary>
-    /// Filtering condition specification
+    /// Specifies a filtering condition for the data query.
     /// </summary>
-    /// <param name="expr">Logical expression for items filtering</param>
-    /// <returns></returns>
+    /// <param name="expr">Logical expression for filtering items.</param>
+    /// <returns>The current specification with the new filtering condition applied.</returns>
     TSpecification Where(Expression<Func<T, bool>> expr);
 
     /// <summary>
-    /// Ascending order specification.
-    /// Any additional Order specification add second factor specification
-    /// and not override the previous one
+    /// Specifies an ascending order for the data query.
+    /// Any additional OrderBy specification adds a secondary factor and does not override the previous one.
     /// </summary>
-    /// <param name="orderByExpression"></param>
+    /// <param name="orderByExpression">The expression to order by in ascending order.</param>
+    /// <returns>The current specification with the new ascending order condition applied.</returns>
     TSpecification OrderBy(Expression<Func<T, object>> orderByExpression);
 
-
     /// <summary>
-    /// Descending order specification.
-    /// Any additional Order specification add second factor specification
-    /// and not override the previous one
+    /// Specifies a descending order for the data query.
+    /// Any additional OrderByDesc specification adds a secondary factor and does not override the previous one.
     /// </summary>
-    /// <param name="orderByDescExpression"></param>
+    /// <param name="orderByDescExpression">The expression to order by in descending order.</param>
+    /// <returns>The current specification with the new descending order condition applied.</returns>
     TSpecification OrderByDesc(Expression<Func<T, object>> orderByDescExpression);
 }
--- DimonSmart.Specification/ISpecificationData.cs
+++ DimonSmart.Specification/ISpecificationData.cs
@@ -2,25 +2,45 @@
 
 namespace DimonSmart.Specification;
 
+/// <summary>
+/// Interface for holding data related to filtering and ordering of query results.
+/// </summary>
+/// <typeparam name="T">The type of entity being queried.</typeparam>
 public interface ISpecificationData<T>
 {
     /// <summary>
-    /// Where clause for data filtering
+    /// Gets the expression representing the WHERE clause for data filtering.
     /// </summary>
+    /// <remarks>
+    /// The expression should evaluate to a Boolean value to filter the query results.
+    /// If not specified, no filtering condition will be applied.
+    /// </remarks>
     Expression<Func<T, bool>>? WhereExpression { get; }
 
     /// <summary>
-    /// OrderBy property and direction list
+    /// Gets the list of order expressions and their corresponding directions.
     /// </summary>
-    public List<(OrderDirectionEnum direction, Expression<Func<T, object>> expr)> OrderExpressions { get; }
+    /// <remarks>
+    /// Each item in the list consists of an expression to order by and the order direction (ascending or descending).
+    /// The order expressions will be applied in the specified order of appearance.
+    /// </remarks>
+    List<(OrderDirectionEnum direction, Expression<Func<T, object>> expr)> OrderExpressions { get; }
 
     /// <summary>
-    /// Specify how many elements should be skipped
+    /// Gets the number of elements to take from the query result.
     /// </summary>
-    public int? TakeQ { get; }
+    /// <remarks>
+    /// If specified, the query will return only the specified number of elements.
+    /// If not specified, all matching elements will be returned.
+    /// </remarks>
+    int? TakeQ { get; }
 
     /// <summary>
-    /// Specify how many elements to take
+    /// Gets the number of elements to skip from the query result.
     /// </summary>
-    public int? SkipQ { get; }
+    /// <remarks>
+    /// If specified, the query will skip the specified number of elements and start from the next one.
+    /// If not specified, no elements will be skipped.
+    /// </remarks>
+    int? SkipQ { get; }
 }
